213ee73faf55000327ec6d37d03d47d731491965
Omar <omarramo.bs@gmail.com>
2021-06-29 16:16:40 +0300

    Trying public/private resources in mylibrary

--- app/src/main/java/com/omar67/librarysample/MainActivity.kt
+++ app/src/main/java/com/omar67/librarysample/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         MyLogger.d("test123")
+        MyLogger.d(getString(R.string.tag2))
+
     }
 }
