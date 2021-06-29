2db227b26911978290a9c0d782da2371743aa54c
Omar <omarramo.bs@gmail.com>
2021-06-29 13:13:11 +0300

    Implemented the library

--- app/src/main/java/com/omar67/librarysample/MainActivity.kt
+++ app/src/main/java/com/omar67/librarysample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.omar67.librarysample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omar67.mylibrary.MyLogger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MyLogger.d("test123")
     }
 }
